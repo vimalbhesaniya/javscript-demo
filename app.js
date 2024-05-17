f7ac017cade064b4e9aed76caeab8763d0fafd0e
vimalbhesaniya <vimalbhesaniya007@gmail.com>
2024-05-17 15:11:53 +0530

    -- 17 may

--- app.js
+++ app.js
@@ -1,151 +1,75 @@
 let dataTable = document.getElementById("data");
-let countryState = []
-let statesState = []
-let cityState = []
+let countryState = [];
+let statesState = [];
+let cityState = [];
+var cid;
+let errorState = []
+let username, gender, isAnyError, chkBox, email;
+let hobbies = [];
+let address = [];
 
-let users = [
-    {
-        name: "jasmin axim",
-        email: "bhesaniya@gmail.com",
-        gender: "male",
-        hobby: ["reading", "sports"],
-        country: "india",
-        state: "gujarat",
-        city: "surat",
-    },
-    {
-        name: "divyesh kubavat",
-        email: "dkubavat@gmail.com",
-        gender: "male",
-        hobby: ["reading"],
-        country: "india",
-        state: "gujarat",
-        city: "surat",
-    },
-];
 
-let country = [
-    {
-        cid: 0,
-        country: "India"
-    },
-    {
-        cid: 1,
-        country: "UnitedStates",
-    },
-    {
-        cid: 2,
-        country: "China",
-    },
-    {
-        cid: 3,
-        country: "UnitedKingdom"
+let clearField = () => {
+    hobbies = []
+    address = []
+    errorState = []
+    countryState = []
+    cityState = []
+    countryState = []
+}
+
+let data = () => {
+    return {
+        name: username?.value,
+        email: email?.value,
+        gender: gender?.value,
+        hobby: hobbies,
+        country: { id: address[0]?.key, value: address[0]?.value },
+        state: { id: address[1]?.key, value: address[1]?.value },
+        city: { id: address[2]?.key, value: address[2]?.value },
     }
-]
+}
 
+function getDataFromFromUser() {
+    username = document.getElementById("name");
+    email = document.getElementById("email");
+    gender = document.querySelector("input[type='radio']:checked");
+    chkBox = document.querySelectorAll("input[type='checkbox']:checked");
 
-let state = [
-    {
-        cid: 0,
-        states: [
-            {
-                sid: 0,
-                state: "Gujarat"
-            },
-            {
-                sid: 1,
-                state: "Maharshtra",
-            },
-            {
-                sid: 2,
-                state: "MadhyaPradesh",
-            },
-            {
-                sid: 3,
-                state: "Rajsthan",
-            }
-        ]
-    },
-    {
-        cid: 1,
-        states: [
-            {
-                sid: 0,
-                state: "California"
-            },
-            {
-                sid: 1,
-                state: "NewYork",
-            },
-            {
-                sid: 2,
-                state: "Texas",
-            },
-            {
-                sid: 3,
-                state: "Florida",
-            }
-        ]
-    },
+    chkBox.forEach((e) => hobbies.push(e.value));
 
-]
+    let selectedAddress = document.querySelectorAll("select");
 
-let city = [
-    {
-        cid: 0,
-        sid: 0,
-        cities: [{ name: "Surat" }, { name: "Rajkot" }, { name: "Ahmdabad" }, { name: "Vadodra" }, { name: "Amreli" }, { name: "Navsari" }, { name: "Kamrej" }]
-    },
-    {
-        cid: 0,
-        sid: 1,
-        cities: [{ name: "Mumbai" }, { name: "Nagpur" }, { name: "Aurangabad" }, { name: "Nashik" }, { name: "Pune" }, { name: "Amravati" }, { name: "Thane" }]
-    },
-    {
-        cid: 0,
-        sid: 2,
-        cities: [{ name: "Gwalior" }, { name: "Orchha" }, { name: "Khajuraho" }, { name: "Omkareshwar" }, { name: "Chitrakoot" }, { name: "Mandu" }, { name: "Jabalpur" }, { name: "Sanchi" }, { name: "Kanha" }, { name: "Ujjain" }, { name: "Panchmarhi" }]
-    },
-    {
-        cid: 0,
-        sid: 3,
-        cities: [{ name: "jaipur" }, { name: "Jodhpur" }, { name: "Kota" }, { name: "Udaipur" }, { name: "Ajmer" }, { name: "Sikar" }, { name: "Pall" }, { name: "Tonk" }]
-    },
-    {
-        cid: 1,
-        sid: 0,
-        cities: [{ name: "Los Angeles" }, { name: "San Francisco" }, { name: "San Diego" }, { name: "Sacramento" }, { name: "San Jose" }, { name: "Oakland" }, { name: "Fresno" }]
-    },
-    {
-        cid: 1,
-        sid: 1,
-        cities: [{ name: "New York City" }, { name: "Buffalo" }, { name: "Rochester" }, { name: "Syracuse" }, { name: "Albany" }, { name: "Yonkers" }, { name: "Utica" }]
-    },
-    {
-        cid: 1,
-        sid: 2,
-        cities: [{ name: "Houston" }, { name: "Dallas" }, { name: "San Antonio" }, { name: "Austin" }, { name: "Fort Worth" }, { name: "El Paso" }, { name: "Arlington" }]
-    },
-    {
-        cid: 1,
-        sid: 3,
-        cities: [{ name: "Miami" }, { name: "Tampa" }, { name: "Orlando" }, { name: "Jacksonville" }, { name: "Fort Lauderdale" }, { name: "Tallahassee" }, { name: "St. Petersburg" }]
-    },
-]
-
-
+    selectedAddress.forEach((e) => {
+        if (e.selectedOptions.item(0).value !== "0") {
+            address.push({
+                key: e.selectedOptions.item(0).value,
+                value: e.selectedOptions.item(0)?.innerHTML,
+            });
+        } else {
+            address.push({
+                key: e.selectedOptions.item(0).value,
+                value: e.selectedOptions.item(0)?.value,
+            });
+        }
+    });
+}
 
 let display = (array) => {
     let target = document.getElementById("data");
     let text = "<tr>";
 
-    array.map((item) => {
+    array.map((item, i) => {
         for (const key in item) {
-            text += `<td>${item[key]}</td>`;
+            if (!Array.isArray(item[key]) && typeof item[key] == "object") {
+                text += `<td>${item[key]["value"]}</td>`;
+            } else {
+                text += `<td>${item[key]}</td>`;
+            }
         }
         text += `<td class='action'>
-        <button class="btnEdit">Edit</button>
-        <button class="btnDelete">Delete</button>
+        <button class="btnEdit" onclick="handleUpdate(${i})">Edit</button>
+        <button class="btnDelete" onclick="handledelete(${i})">Delete</button>
        </td>`;
         text += `</tr>`;
     });
@@ -153,120 +77,259 @@ let display = (array) => {
     target.innerHTML = text;
 };
 
-
-
-let countrySelectBox = document.getElementById("country")
-let stateSelectBox = document.getElementById("state")
-let citySelectBox = document.getElementById("city")
-
+let countrySelectBox = document.getElementById("country");
+let stateSelectBox = document.getElementById("state");
+let citySelectBox = document.getElementById("city");
 
 //get country
 let getCountry = () => {
-    return country.map(e => e)
-}
+    return country.map((e) => e);
+};
 //get state
 let getStateForContry = (id) => {
-    return state.filter((e) => {
-        if (id == e.cid) {
-            return e.states
-        }
-    }).map((e) => e.states)
-}
+    return state
+        .filter((e) => {
+            if (id == e.cid) {
+                return e.states;
+            }
+        })
+        .map((e) => e.states);
+};
 
 //get city
 let getCityForState = (cid, sid) => {
-    return city.filter((e) => {
-        if (e.sid == sid && e.cid == cid) {
-            return e
-        }
-    }).map((e) => e.cities)
-}
+    return city
+        .filter((e) => {
+            if (e.sid == sid && e.cid == cid) {
+                return e;
+            }
+        })
+        .map((e) => e.cities);
+};
 
 //handle select box
-let handleSelection = (array, selectedBox, val, text) => {
-    let bind = ''
+let handleSelection = (array, selectedBox, val, text, title, valueToSelect) => {
+    let bind = `<option value="0" ${(val == "0") & "selected"}>${title}</option>`;
     array?.forEach((e) => {
-        bind += `<option value='${e[val]}' >${e[text]}</option>`
-    })
-    selectedBox.innerHTML = bind
-}
-
-
+        bind += `<option value='${e[val]}' ${valueToSelect == e[val] && "selected"
+            } >${e[text]}</option>`;
+    });
+    selectedBox.innerHTML = bind;
+};
 //country handling...
-var cid;
 countrySelectBox.addEventListener("change", (event) => {
     cid = event.target.value;
-    let allState = getStateForContry(cid)[0]
-    handleSelection(allState, stateSelectBox, 'sid', 'state')
-    citySelectBox.innerHTML = ''
-})
+    let allState = getStateForContry(cid)[0];
+    handleSelection([], stateSelectBox, "", "", "Select State");
+    handleSelection(allState, stateSelectBox, "sid", "state", "Select State");
+    handleSelection([], citySelectBox, "", "", "Select City");
+});
 
 //state handling...
 stateSelectBox.addEventListener("change", (event) => {
-    let sid = event.target.value
-    let cities = getCityForState(cid, sid)
-    handleSelection(cities[0], citySelectBox, 'name', 'name')
-})
-
-
+    let sid = event.target.value;
+    let cities = getCityForState(cid, sid);
+    handleSelection(cities[0], citySelectBox, "id", "name", "Select City");
+});
 
 //when page is load then...
-handleSelection(country, countrySelectBox, 'cid', 'country')
+handleSelection(country, countrySelectBox, "cid", "country", "Select Country");
+handleSelection([], stateSelectBox, "", "", "Select State");
+handleSelection([], citySelectBox, "", "", "Select City");
 display(users);
 
-
-
 //validations
-document.getElementById("email").addEventListener("blur" , (event) =>{
-    let validEmail = event.target.value;
-    let error = document.querySelectorAll(".error").item(1)
-    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    error.innerHTML =  !emailRegex.test(validEmail) ?"Please Enter valid email" : "" 
-})
 
+let checkEmail = (email, whereToShow) => {
+    let error = document.getElementById(whereToShow);
+    let emailRegex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.length == 0) {
+        error.innerHTML = "Email is required";
+        return false;
+    } else if (!emailRegex.test(email)) {
+        error.innerHTML = "Please enter a valid email ";
+        return false;
+    } else {
+        return true;
+    }
+};
 
-let checkInputs = ( errMsg , value , whereToShow ) =>{
-    console.log(value , whereToShow);
-    if (value)  {
-        document.getElementById(whereToShow).innerHTML = "";
-        return true
+let checkInputs = (errMsg, value, whereToShow, type) => {
+    let where = document.getElementById(whereToShow);
+    switch (type) {
+        case "text":
+            if (value.trim().length == 0) {
+                where.innerHTML = errMsg;
+                return false;
+            } else {
+                where.innerHTML = "";
+                return true;
+            }
+            break;
+        case "email":
+            return checkEmail(value, whereToShow);
+            break;
+        case "radio":
+            if (!value) {
+                where.innerHTML = errMsg;
+                return false;
+            } else {
+                where.innerHTML = "";
+                return true;
+            }
+            break;
+        case "select":
+            if (value == "0") {
+                console.log(value);
+                where.innerHTML = errMsg;
+                return false;
+            } else {
+                where.innerHTML = "";
+                return true;
+            }
+            break;
+        case "checkbox":
+            if (value.length == 0) {
+                where.innerHTML = errMsg;
+                return false;
+            } else {
+                where.innerHTML = "";
+                return true;
+            }
+            break;
+        default:
+            return false;
+            break;
     }
-    else{
-        document.getElementById(whereToShow).innerHTML = errMsg;
-        return false
-    }
+};
+
+let checkErrors = () => {
+
+    return errorState = [
+        checkInputs(msg, username.value, "nameError", "text"),
+        checkInputs(msg, email.value, "emailError", "email"),
+        checkInputs(msg, gender?.value, "genderError", "radio"),
+        checkInputs(msg, hobbies, "hobbyError", "checkbox"),
+        checkInputs(msg, address[0].value, "countryError", "select"),
+        checkInputs(msg, address[1].value, "stateError", "select"),
+        checkInputs(msg, address[2].value, "cityError", "select"),
+    ];
+
 }
 
-
 //when user submitng form...
+
+let msg = "This field is required";
 document.getElementById("form").addEventListener("submit", (event) => {
     event.preventDefault();
+    getDataFromFromUser();
 
-    let [name , email] = document.forms["form"];
-    let gender = document.querySelector("input[type='radio']:checked")?.value
-    let chkBox = document.querySelectorAll("input[type='checkbox']:checked");
-    
-    let hobbies = [];
-    let address = [];
+    isAnyError = checkErrors().every((err) => (err ? 1 : 0));
 
-    chkBox.forEach((e) => hobbies.push(e.value));
+    if (isAnyError) {
+        users = [
+            ...users,
+            data(),
+        ];
+        form.reset();
+        console.log(users);
+    }
 
-    let selectedAddress = document.querySelectorAll("select");
-    console.log(selectedAddress);
-    selectedAddress.forEach((e) => {
-        address.push(e.selectedOptions.item(0)?.innerHTML)
-    })
+    display(users);
+    clearField()
+});
 
-    let msg = "This field is required"
-        users = [...users, {
-            name: name.value,
-            email: email.value,
-            gender: gender,
-            hobby: hobbies,
-            country: address[0],
-            state: address[1],
-            city: address[2],
-        }]
-        display(users)
+let handledelete = (id) => {
+    users.splice(id, 1);
+    display(users);
+};
 
-})
+let handleUpdate = (id) => {
+    save.classList.add("hide");
+    edit.classList.remove("hide");
+    cancle.classList.remove("hide");
+    cancle.addEventListener("click", () => {
+        save.classList.remove("hide");
+        edit.classList.add("hide");
+        cancle.classList.add("hide");
+    });
+
+    username.value = users[id].name;
+    email.value = users[id].email;
+
+    let radio = document.querySelectorAll("input[type=radio]");
+    radio.forEach((e) => {
+        if (e.value == users[id].gender) {
+            e.checked = true;
+        }
+    });
+
+    let checkBox = document.querySelectorAll("input[type=checkbox]");
+    checkBox.forEach((e) => {
+        if (users[id].hobby.includes(e.value)) {
+            e.checked = true;
+        } else {
+            e.checked = false;
+        }
+    });
+
+    handleSelection(
+        country,
+        countrySelectBox,
+        "cid",
+        "country",
+        "Select Country",
+        users[id].country.id
+    );
+
+    let allState = getStateForContry(users[id].country.id)[0];
+
+    handleSelection(
+        allState,
+        stateSelectBox,
+        "sid",
+        "state",
+        "Select State",
+        users[id].state.id
+    );
+
+    let cities = getCityForState(users[id].country.id, users[id].state.id);
+
+    handleSelection(
+        cities[0],
+        citySelectBox,
+        "id",
+        "name",
+        "Select City",
+        users[id].city.id
+    );
+
+    document.getElementById("edit").addEventListener("click", () => {
+        getDataFromFromUser();
+
+        isAnyError = checkErrors().every((err) => (err ? 1 : 0));
+        if (isAnyError) {
+            users = users.with(
+                id,
+                data()
+            )
+            display(users)
+            clearField()
+        }
+    });
+};
+
+let shortName = () => {
+    console.log(users.sort((a, b) => {
+        if (a.name < b.name)
+            return -1;
+        else if (a.name == b.name)
+            return 0;
+        else
+            return 1;
+    }));
+
+}
+
+shortName()
---
+++ data.js
@@ -0,0 +1,196 @@
+let users = [
+  {
+    name: "jasmin axim",
+    email: "bhesaniya@gmail.com",
+    gender: "male",
+    hobby: ["reading", "sports"],
+    country: "india",
+    state: "gujarat",
+    city: "surat",
+  },
+  {
+    name: "divyesh kubavat",
+    email: "dkubavat@gmail.com",
+    gender: "male",
+    hobby: ["reading"],
+    country: "india",
+    state: "gujarat",
+    city: "surat",
+  },
+];
+
+let country = [
+  {
+    cid: 1,
+    country: "India",
+  },
+  {
+    cid: 2,
+    country: "UnitedStates",
+  },
+  {
+    cid: 3,
+    country: "China",
+  },
+  {
+    cid: 4,
+    country: "UnitedKingdom",
+  },
+];
+
+let state = [
+  {
+    cid: 1,
+    states: [
+      {
+        sid: 1,
+        state: "Gujarat",
+      },
+      {
+        sid: 2,
+        state: "Maharshtra",
+      },
+      {
+        sid: 3,
+        state: "MadhyaPradesh",
+      },
+      {
+        sid: 4,
+        state: "Rajsthan",
+      },
+    ],
+  },
+  {
+    cid: 2,
+    states: [
+      {
+        sid: 1,
+        state: "California",
+      },
+      {
+        sid: 2,
+        state: "NewYork",
+      },
+      {
+        sid: 3,
+        state: "Texas",
+      },
+      {
+        sid: 4,
+        state: "Florida",
+      },
+    ],
+  },
+];
+
+let city = [
+  {
+    cid: 1,
+    sid: 1,
+    cities: [
+      { id : 1 ,  name: "Surat" },
+      { id : 2 ,  name: "Rajkot" },
+      { id : 3 ,  name: "Ahmdabad" },
+      { id : 4 ,  name: "Vadodra" },
+      { id : 5 ,  name: "Amreli" },
+      { id : 6 ,  name: "Navsari" },
+      { id : 7 ,  name: "Kamrej" },
+    ],
+  },
+  {
+    cid: 1,
+    sid: 2,
+    cities: [
+      {  id : 1 , name: "Mumbai" },
+      {  id : 2 , name: "Nagpur" },
+      {  id : 3 , name: "Aurangabad" },
+      {  id : 4 , name: "Nashik" },
+      {  id : 5 , name: "Pune" },
+      {  id : 6 , name: "Amravati" },
+      {  id : 7 , name: "Thane" },
+    ],
+  },
+  {
+    cid: 1,
+    sid: 2,
+    cities: [
+      { id : 1 , name: "Gwalior" },
+      { id : 2 , name: "Orchha" },
+      { id : 3 , name: "Khajuraho" },
+      { id : 4 , name: "Omkareshwar" },
+      { id : 5 , name: "Chitrakoot" },
+      { id : 6 , name: "Mandu" },
+      { id : 7 , name: "Jabalpur" },
+      { id : 9 , name: "Sanchi" },
+      { id : 10,  name: "Kanha" },
+      { id : 11 , name: "Ujjain" },
+      {  id : 12  , name: "Panchmarhi" },
+    ],
+  },
+  {
+    cid: 1,
+    sid: 2,
+    cities: [
+      {  id : 1 , name: "jaipur" },
+      {  id : 2 , name: "Jodhpur" },
+      {  id : 3 , name: "Kota" },
+      {  id : 4 , name: "Udaipur" },
+      {  id : 5 , name: "Ajmer" },
+      {  id : 6 , name: "Sikar" },
+      {  id : 7 , name: "Pall" },
+      { id : 8 , name: "Tonk" },
+    ],
+  },
+  {
+    cid: 2,
+    sid: 1,
+    cities: [
+      {  id : 1 ,  name: "Los Angeles" },
+      {  id : 2 ,  name: "San Francisco" },
+      {  id : 3 ,  name: "San Diego" },
+      {  id : 4 ,  name: "Sacramento" },
+      {  id : 5 ,  name: "San Jose" },
+      {  id : 6 ,  name: "Oakland" },
+      {  id : 7 ,  name: "Fresno" },
+    ],
+  },
+  {
+    cid: 2,
+    sid: 2,
+    cities: [
+      { name: "New York City" },
+      { name: "Buffalo" },
+      { name: "Rochester" },
+      { name: "Syracuse" },
+      { name: "Albany" },
+      { name: "Yonkers" },
+      { name: "Utica" },
+    ],
+  },
+  {
+    cid: 2,
+    sid: 3,
+    cities: [
+      { name: "Houston" },
+      { name: "Dallas" },
+      { name: "San Antonio" },
+      { name: "Austin" },
+      { name: "Fort Worth" },
+      { name: "El Paso" },
+      { name: "Arlington" },
+    ],
+  },
+  {
+    cid: 2,
+    sid: 4,
+    cities: [
+      { name: "Miami" },
+      { name: "Tampa" },
+      { name: "Orlando" },
+      { name: "Jacksonville" },
+      { name: "Fort Lauderdale" },
+      { name: "Tallahassee" },
+      { name: "St. Petersburg" },
+    ],
+  },
+];
